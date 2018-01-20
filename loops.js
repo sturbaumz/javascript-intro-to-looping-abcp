fdc6ddb87101dbceb0c5fcd8376982e4abd57776
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-20 03:15:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ for (let i=0;i<25;i++) {
   if (i===1) {
     array.push('I am 1 strange loop.')
   }
-  else {array.push('I am ${i} strange loops.')
+  else {array.push('I am ${i} strange loop${i===0?'':'s'}.')
 
 }
 return array
